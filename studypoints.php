d9b881a69e3e097bfcc6a95a6597cc84c4b012e0
gopherproxy <gopherproxy@gmail.com>
2017-03-23 15:58:49 +0100

    Study point assigment
    small fix

--- studypoints.php
+++ studypoints.php
@@ -12,7 +12,7 @@
 <!-- Output depending on the number of gathered study points: -->
 <!-- Less than 80 study points red text stating: Sorry. Just [number of study points] sp are not enough. You miss [number of study points student is missing] sp -->
 <!-- 80 or more study points green text: Very good. With [number of study points] sp you can go to the exam! -->
-<!-- More than 100 study points red text: [number of study points] sp? Cheater! Or genius. ->
+<!-- More than 100 study points red text: [number of study points] sp? Cheater! Or genius. -->
 <!-- NO INLINE STYLES -->
 <!-- Test your program routine with the numbers 75, 80, 85, 100, 200 -->
 </body>
