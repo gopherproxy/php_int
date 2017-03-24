4a3ebc9202c1970b84369f0e49ca7b5e90894db6
gopherproxy <gopherproxy@gmail.com>
2017-03-24 09:46:44 +0100

    Study points assignment solved
    + user input

--- studypoints.php
+++ studypoints.php
@@ -1,8 +1,33 @@
+<?php 
+// the global variables I need
+// filter_input grabs external variables (f.i. form fields) and can FILTER them!
+$sp = filter_input(INPUT_GET, 'sp');
+$studypoints = $sp;
+$spRest = 80 - $studypoints;
+
+// set og variables needed for HTML output
+
+$pGstart = '<p class="green">';
+$pRstart = '<p class="red">';
+$pStop = '</p>';
+
+// user feedback
+// double quoted strings can output variables directly!
+$spMinus = "$studypoints studypoints are not enough. You miss $spRest studypoints!";
+$spOk = "With $studypoints studypoints you can attend the exam!";
+$spPlus = "$studypoints studypoints? Come on - that's not likely."
+
+
+?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
 <title>Study points assignment</title>
+<style type="text/css">
+	.green {color : green;}
+	.red {color : red;}
+</style>
 </head>
 
 <body>
@@ -15,5 +40,19 @@
 <!-- More than 100 study points red text: [number of study points] sp? Cheater! Or genius. -->
 <!-- NO INLINE STYLES -->
 <!-- Test your program routine with the numbers 75, 80, 85, 100, 200 -->
+<form>
+	<input type="number" name="sp" required>
+	<input type="submit" name="submit" value="so what?">
+</form>
+<?php 
+// if less than 80 points:
+if($studypoints < 80){
+ echo $pRstart . $spMinus . $pStop;
+} else if($studypoints <= 100){
+ echo $pGstart . $spOk . $pStop;
+}else{
+ echo $pRstart . $spPlus . $pStop;
+}
+?>
 </body>
 </html>
