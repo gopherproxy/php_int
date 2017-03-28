697b3f3ffec638e6dca1b7e630eeda47a4c06d7d
Tue Becher <tue@Tues-Mac.local>
2017-03-28 15:18:37 +0200

    Added db connection

---
+++ db_con.php
@@ -0,0 +1,17 @@
+<?php
+
+const HOSTNAME  = 'localhost'; // server
+const MYSQLUSER = 'root'; // database user
+const MYSQLPASS = 'root'; // database password
+const MYSQLDB   = 'mil'; // database name
+
+// Create database connection object
+$con = new mysqli(HOSTNAME, MYSQLUSER, MYSQLPASS, MYSQLDB);
+
+// fail on error
+if ($con->connect_error){
+	die('Error: '.$con->connect_error.' ('.$con->connect_errno.')');
+}
+
+// set charset utf8 to match coallation in db
+$con->set_charset('utf8');
